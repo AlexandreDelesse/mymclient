1c18c3a4bff9dc4b7e3d3136438b71f6856f69e4
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2024-12-18 12:38:14 +0100

    add version display

--- src/components/ingredient/IngredientPage.tsx
+++ src/components/ingredient/IngredientPage.tsx
@@ -1,33 +1,34 @@
-import React, { useState } from "react";
-import IngredientList from "./IngredientList";
-import { Box, Button, Stack, TextField } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import CloseIcon from "@mui/icons-material/Close";
-import IngredientForm from "./IngredientForm";
+import React, { useState } from 'react'
+import IngredientList from './IngredientList'
+import { Box, Button, Stack, TextField } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import CloseIcon from '@mui/icons-material/Close'
+import IngredientForm from './IngredientForm'
+import packageJson from '../../../package.json'
 
 export default function IngredientPage() {
-  const [formOpen, setFormOpen] = useState(false);
-  const [filter, setFilter] = useState("");
+  const [formOpen, setFormOpen] = useState(false)
+  const [filter, setFilter] = useState('')
 
-  const toggleFormOpen = () => setFormOpen(!formOpen);
+  const toggleFormOpen = () => setFormOpen(!formOpen)
 
   return (
     <Box sx={{ marginTop: 4 }}>
-      <Box sx={{display: "flex", gap: 2}}>
+      <Box sx={{ display: 'flex', gap: 2 }}>
         <TextField
           size="small"
           label="Rechercher"
-          sx={{flexGrow: 1}}
+          sx={{ flexGrow: 1 }}
           value={filter}
           onChange={(e) => setFilter(e.target.value)}
         />
         <Button
-          variant={formOpen ? "outlined" : "contained"}
+          variant={formOpen ? 'outlined' : 'contained'}
           startIcon={formOpen ? <CloseIcon /> : <AddIcon />}
           onClick={toggleFormOpen}
-          color={formOpen ? "warning" : "primary"}
+          color={formOpen ? 'warning' : 'primary'}
         >
-          {formOpen ? "Annuler" : "Ajouter"}
+          {formOpen ? 'Annuler' : 'Ajouter'}
         </Button>
       </Box>
 
@@ -35,6 +36,7 @@ export default function IngredientPage() {
         {formOpen && <IngredientForm onSave={toggleFormOpen} />}
         {!formOpen && <IngredientList filter={filter} />}
       </Box>
+      <Box>Version : {packageJson.version}</Box>
     </Box>
-  );
+  )
 }
